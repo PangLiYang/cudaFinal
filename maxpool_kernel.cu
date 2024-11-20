432f13fee8dbb80cbd61b740bbe9c59e022031a2
PangLi Yang <reli810436@gmail.com>
2024-11-20 00:22:47 -0500

    v1

--- maxpool_kernel.cu
+++ maxpool_kernel.cu
@@ -66,14 +66,6 @@ int main() {
 
     cout << "max-pooling time = " << measured_time / times * 1000 << " ms" <<endl;
 
-//    for (int i = 0; i < dim_out; i += 1) {
-//        cout << "value[" << i << "] = " << *(value + i) << endl;
-//    }
-//
-//    for (int i = 0; i < dim_out; i += 1) {
-//        cout << "indices[" << i << "] = " << *(indices + i) << endl;
-//    }
-
     cudaFree(data);
     cudaFree(value);
     cudaFree(indices);
